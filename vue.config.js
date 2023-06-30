804f57740f287c0311b7cadc10448d4c86950d75
LuMi2602 <wedimulie@gmail.com>
2023-06-30 19:22:31 +0400

    update

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath:"CW3_Components",
+  publicPath:'/CW3_Components/',
   transpileDependencies: true
 })
